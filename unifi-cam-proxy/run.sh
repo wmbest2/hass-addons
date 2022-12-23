1546876ca59c03d0b9552d8526d7efad742abe1a
Bill <bill.best@numerator.com>
2022-12-23 07:23:56 -0500

    Fix variable name

--- unifi-cam-proxy/run.sh
+++ unifi-cam-proxy/run.sh
@@ -13,7 +13,7 @@ FFMPEG_PARAMS="$(bashio::config 'ffmpeg_params')"
 temp_file=$(mktemp)
 trap "rm -f $temp_file" 0 2 3 15
 
-echo $CERT >> $tmp_file
+echo $CERT >> $temp_file
 
 unifi-cam-proxy -H $NVR_IP -i $CAM_IP -c $temp_file -t $TOKEN \
     amcrest \
